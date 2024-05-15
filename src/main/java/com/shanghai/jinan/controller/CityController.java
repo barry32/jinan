8a39f45d46c77e3c710d9c421110262b1e4abd6f
barry32 <930562743@qq.com>
2024-05-15 20:59:07 +0800

    'test'

--- src/main/java/com/shanghai/jinan/controller/CityController.java
+++ src/main/java/com/shanghai/jinan/controller/CityController.java
@@ -4,6 +4,7 @@ import com.shanghai.jinan.service.IDestinationBizService;
 import com.shanghai.jinan.vo.ApiResponse;
 import com.shanghai.jinan.vo.CityVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +17,13 @@ public class CityController {
     @Autowired
     private IDestinationBizService iDestinationBizService;
 
-    @PostMapping("/items")
+    @GetMapping("/items")
     public ApiResponse<List<CityVo>> getCityList() {
         List<CityVo> cityList = iDestinationBizService.getCityList();
         return new ApiResponse<>(ApiResponse.RS_CODE_SUCCESS, "success",cityList);
     }
 
-    @PostMapping("/delayed/items")
+    @GetMapping("/delayed/items")
     public ApiResponse<List<CityVo>> getCityListDelayed() throws InterruptedException {
         Thread.sleep(2000);
         List<CityVo> cityList = iDestinationBizService.getCityList();
