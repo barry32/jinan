075935f243e02cae6016caa917cb4189375b4383
barry32 <930562743@qq.com>
2024-05-16 10:59:09 +0800

    修改redis配置

--- src/main/java/com/shanghai/jinan/utils/RedisService.java
+++ src/main/java/com/shanghai/jinan/utils/RedisService.java
@@ -1,21 +1,17 @@
 package com.shanghai.jinan.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisService {
-    @Autowired
-    private RedisTemplate redisTemplate;
 
-    public void setKeyValue(String key, Object value) {
-        redisTemplate.opsForValue().set(key, value);
+    public void setKeyValue(String key, String value) {
+        JedisUtils.getJedis().set(key, value);
     }
 
-    public Object getValueByKey(String key) {
+    public String getValueByKey(String key) {
         if (key != null) {
-            return redisTemplate.opsForValue().get(key);
+            return JedisUtils.getJedis().get(key);
         }
         return null;
     }
