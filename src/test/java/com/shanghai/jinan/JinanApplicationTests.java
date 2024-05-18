1c1c5f7b62db853e9758d62e49f979a952fc2b25
barry32 <930562743@qq.com>
2024-05-18 23:26:08 +0800

    NIO 优化。

--- src/test/java/com/shanghai/jinan/JinanApplicationTests.java
+++ src/test/java/com/shanghai/jinan/JinanApplicationTests.java
@@ -1,13 +1,12 @@
 package com.shanghai.jinan;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JinanApplicationTests {
 
-	@Test
+/*	@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
